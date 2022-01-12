1769465784483750a7d3fe773e0513587f76e96b
Daniel Rozmajzl <daniel.rozmajzl@gmail.com>
2022-01-11 21:16:23 -0500

    commit tree a1140bda0b2061933b181a44cb2e2a3b44225e28

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(x){
+    bestCustomer = x;
+}
+
+const leastFavoriteCustomer = 'a'
+
+function changeLeastFavoriteCustomer(x){
+    leastFavoriteCustomer = x;
+
+}
